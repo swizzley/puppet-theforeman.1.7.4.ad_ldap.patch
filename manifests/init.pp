83cd77d230861778f1d2ff0dc0ad77220715a2e0
Dustin Morgan <morgan@aspendenver.org>
2015-08-07 14:42:47 -0600

    fixed

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,25 +3,28 @@
 
 class theforeman_182_ad_ldap_patch {
   # [param]: $your_ca = your exported CA certificate using BASE-64 format for your ldap provider in swizzley88-theforemanpr132patch 
-  $your_ca = ['some_org.cer']
-  $cert_source = "puppet:///theforemanpr132patch/${your_ca}"
-  $patch = "ln -s /etc/pki/tls/certs/${your_ca} $(openssl x509 -noout -hash -in /etc/pki/tls/certs/${your_ca}).0 "
+  $your_ca = 'RootCA.cer'
+  $cert_source = "puppet:///modules/theforeman_183_ad_ldap_patch/${your_ca}"
+  $patch = "ln -s /etc/pki/tls/certs/${your_ca} /etc/pki/tls/certs/$(openssl x509 -noout -hash -in /etc/pki/tls/certs/${your_ca}).0 "
 
   file { "/pki/tls/certs/${your_ca}":
     ensure  => 'present',
     content => $cert_source,
     owner   => 'root',
     mode    => '0644',
+    require => Package['foreman-installer'],
   } ->
-  exec { "patch_theforeman_1.8.2_ad_ldap_provider":
-    shell   => 'bash',
-    path    => '/bin:/usr/bin:/pki/tls:/etc/pki/tls/certs',
-    command => "unlink /etc/pki/tls/certs/ca-bundle.crt && ${patch}",
-    unless  => "test -L /etc/pki/tls/certs/${your_ca}.0",
+  exec { "patch_comcast_ca_cert":
+    provider => 'shell',
+    path     => '/bin:/usr/bin:/pki/tls:/etc/pki/tls/certs',
+    command  => "unlink /etc/pki/tls/certs/ca-bundle.crt && ${patch}",
+    unless   => "test -L /etc/pki/tls/certs/$(openssl x509 -noout -hash -in /etc/pki/tls/certs/${your_ca}).0",
   } ->
-  file { '/etc/pki/tls/certs/ca-bundle.crt':
-    ensure => 'link',
-    target => "/etc/pki/tls/certs/${your_ca}.0",
+  exec { "relink_ca_cert":
+    provider => 'shell',
+    path     => '/bin:/usr/bin:/pki/tls:/etc/pki/tls/certs',
+    command  => "ln -s /etc/pki/tls/certs/$(openssl x509 -noout -hash -in /etc/pki/tls/certs/${your_ca}).0 /etc/pki/tls/certs/ca-bundle.crt",
+    unless   => 'test -L /etc/pki/tls/certs/ca-bundle.crt',
   }
 
 }
